60ad051645bdac22268209ecda4936d8bd9d49cc
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-07-13 23:27:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/ref', refRoutes);
 
-  
+   
     app.listen(7000, () => {
         console.log(`Server running on port 7000`);
     });
