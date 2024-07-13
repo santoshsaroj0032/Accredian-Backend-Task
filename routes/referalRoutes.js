9736030750a401b59a4744aaa7f23259b74a8db8
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-07-13 23:26:12 +0530

    Update referalRoutes.js

--- routes/referalRoutes.js
+++ routes/referalRoutes.js
@@ -9,11 +9,7 @@ router.patch('/updateref',updateRefs)
 module.exports = router;
 
 
-
-
-// const express = require('express');
-// const router = express.Router();
-
+ 
 // Define your routes here
 router.get('/', (req, res) => {
   res.send('Hello from referral routes!');
